9b0477d667a76c7dfbe94ae57215e6663b93fa90
nurmatov0228 <bahodirnur06@gmail.com>
2024-11-04 14:47:29 -0800

    home cars

--- src/Components/UI/Card/Card.jsx
+++ src/Components/UI/Card/Card.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import "./card.scss";
 
-const Card = ({ item }) => {
+const Card = ({ item, location }) => {
+  const body = () => {
+    if (location === "budget") {
+      return (
+        <div className="card__container">
+          <div className="card__image-wrapper">
+            <img
+              className="card__image"
+              src={`https://realauto.limsa.uz/api/uploads/images/${item?.car_images[0]?.image?.src}`}
+              alt={item?.name_en}
+            />
+          </div>
+          <h4 className="card__title">
+            {item?.brand?.title} {item?.model?.name}
+          </h4>
+          <div className="card__price">
+            <span>AED 0</span>
+            <span>$ 0</span>
+          </div>
+          <p className="card__text">per day</p>
+        </div>
+      );
+    } else {
+      return <h2>not found</h2>;
+    }
+  };
+
   return (
     <div className="card">
-      <div className="container">
-        <div className="card__container">
-          <img src={item.imageUrl} alt={item.name} className="card__image" />
-          <div className="card__details">
-            <h3 className="card__name">{item.name}</h3>
-            <p className="card__price">AED {item.price} / per day</p>
-          </div>
-        </div>
-      </div>
+      <div className="container">{body()}</div>
     </div>
   );
 };
--- src/Components/UI/SportCars/SportCars.jsx
+++ src/Components/UI/SportCars/SportCars.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./budgetcars.scss";
 import axios from "axios";
 import Card from "../Card/Card";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -23,15 +22,15 @@ const BudgetCars = () => {
     fetchData();
   }, []);
 
-  const budgetCars = base.filter(
-    (item) => item?.category?.name_en === "Budget cars Rental Emirates"
+  const sportCars = base.filter(
+    (item) => item?.category?.name_en === "Sports cars Rental Emirates"
   );
 
   return (
     <div className="budget">
       <div className="budget__container">
         <div className="budget__header">
-          <h1>Budget Cars Rental Emirates</h1>
+          <h1>Sports cars Rental Emirates</h1>
           <button className="budget__see-all">See All &gt;</button>
         </div>
         <Swiper
@@ -42,7 +41,7 @@ const BudgetCars = () => {
             clickable: true,
           }}
         >
-          {budgetCars.map((item) => (
+          {sportCars.map((item) => (
             <SwiperSlide>
               <Card key={item.id} item={item} location={"budget"} />
             </SwiperSlide>
