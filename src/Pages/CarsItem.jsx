49a43fd3f06f08d5b983f528bb1535534e351d35
nurmatov0228 <bahodirnur06@gmail.com>
2024-11-06 14:51:39 -0800

    caritem add

---
+++ src/Pages/CarsItem.jsx
@@ -0,0 +1,109 @@
+// import React from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import { Navigation } from "swiper/modules"; // 'swiper' dan to'g'ri modulni import qiling
+// import {
+//   FaWhatsapp,
+//   FaTelegramPlane,
+//   FaPhone,
+//   FaCar,
+//   FaCalendarAlt,
+//   FaTachometerAlt,
+//   FaCogs,
+//   FaGasPump,
+//   FaUsers,
+//   FaPalette,
+// } from "react-icons/fa";
+// import { GiSteeringWheel } from "react-icons/gi";
+// import "../styles/caritem.scss";
+
+// const CarItem = () => {
+//   return (
+//     <div className="car-item">
+//       <h1>MITSUBISHI MIRAGE (WHITE)</h1>
+
+//       {/* Slayder qismi */}
+//       <div className="car-slider">
+//         <Swiper modules={[Navigation]} navigation slidesPerView={1}>
+//           <SwiperSlide>
+//             <img src="image1.png" alt="Car Image 1" />
+//           </SwiperSlide>
+//           <SwiperSlide>
+//             <img src="image2.png" alt="Car Image 2" />
+//           </SwiperSlide>
+//           <SwiperSlide>
+//             <img src="image3.png" alt="Car Image 3" />
+//           </SwiperSlide>
+//         </Swiper>
+
+//         {/* Thumbnail rasmlar */}
+//         <div className="thumbnails">
+//           <img src="image1.png" alt="Thumbnail 1" />
+//           <img src="image2.png" alt="Thumbnail 2" />
+//           <img src="image3.png" alt="Thumbnail 3" />
+//         </div>
+//       </div>
+
+//       {/* Narx va ma'lumot */}
+//       <div className="price-info">
+//         <p>AED 0 / $0</p>
+//         <p className="crossed-price">AED 5</p>
+//         <p>per day</p>
+//       </div>
+
+//       {/* Avtomobil xususiyatlari */}
+//       <div className="car-specs">
+//         <div>
+//           <FaCalendarAlt className="spec-icon" /> 2022
+//         </div>
+//         <div>
+//           <FaTachometerAlt className="spec-icon" /> 7
+//         </div>
+//         <div>
+//           <FaCogs className="spec-icon" /> 250
+//         </div>
+//         <div>
+//           <FaUsers className="spec-icon" /> 4
+//         </div>
+//         <div>
+//           <FaPalette className="spec-icon" /> White
+//         </div>
+//       </div>
+
+//       <div className="car-details">
+//         <div>
+//           <FaCar className="detail-icon" /> v4
+//         </div>
+//         <div>
+//           <FaCogs className="detail-icon" /> Automatic box
+//         </div>
+//         <div>
+//           <GiSteeringWheel className="detail-icon" /> Left
+//         </div>
+//         <div>
+//           <FaGasPump className="detail-icon" /> Petrol
+//         </div>
+//         <div>Budget cars</div>
+//       </div>
+
+//       {/* Aloqa tugmalari */}
+//       <div className="contact-icons">
+//         <FaWhatsapp className="icon whatsapp" />
+//         <FaTelegramPlane className="icon telegram" />
+//         <FaPhone className="icon phone" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CarItem;
+
+import React from "react";
+import "../styles/caritem.scss";
+
+const CarsItem = () => {
+  return <div></div>;
+};
+
+export default CarsItem;
