5d1a4b6c03e43dd00b764b8e0defb1dd25f424d5
yo1doshevv <yoldoshevbehruz701@gmail.com>
2024-11-08 16:07:29 +0500

    blog page tugadi

---
+++ src/Pages/Brend.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Brend = () => {
+  return (
+    <div>Brend</div>
+  )
+}
+
+export default Brend
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,13 +4,23 @@ import Home from "../Pages/Home";
 import About from "../Pages/About";
 import Contact from "../Pages/Contact";
 import CarsPage from "../Pages/Cars";
+import Brend from "../Pages/Brend";
+import Blog from "../Pages/Blog";
+import BlogView2 from "../Pages/BlogView2";
+import BlogView3 from "../Pages/BlogView3";
+import BlogVew1 from "../Pages/BlogView1";
 function Router() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blogview1" element={<BlogVew1 />} />
+        <Route path="/blogview2" element={<BlogView2/>} />
+        <Route path="/blogview3" element={<BlogView3 />} />
+        <Route path="/brand" element={<Brend/>}/>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog/>}/>
         <Route path="/cars" element={<CarsPage />} />
       </Routes>
     </div>
